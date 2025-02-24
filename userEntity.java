da5d1cff2883b894799a02756633584f28a19628
RakeshSubramani <jhonrakeshcena@gmail.com>
2025-02-24 09:48:31 +0530

    Create userEntity added

---
+++ userEntity.java
@@ -0,0 +1,12 @@
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Getters and Setters
+}
